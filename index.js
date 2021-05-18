19c200790a8c71ae08fa0be8fd540066ca0076c9
NickoAcu <nickoacu@hotmail.com>
2021-05-18 01:19:20 -0300

    Add: Se añadieron los comandos [hola] y [help].

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ client.on("ready", () => {
     console.log(`Su estado actual es ${client.user.presence.status}`);
   });
 
+client.on("message", message => {
+  if (message.author.bot) return;
+  // Filtrado de mensajes por el simbolo identificador de comandos
+  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+  // Parseado de argumentos de los comandos
+  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // Comandos de prueba
+  if(command === 'hola') {
+    message.channel.send(`Hola ${message.author.username}!`);
+  } else
+  if (command === 'help') {
+    message.channel.send(`Por ahora el unico comando que tengo habilitado es ${process.env.PREFIX}hola`);
+  }
+});
+
 client.login()
