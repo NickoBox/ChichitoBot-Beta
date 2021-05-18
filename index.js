bfa502b342ce51b397637822f248b209b5a0a309
NickoAcu <nickoacu@hotmail.com>
2021-05-18 02:39:16 -0300

    Update: Se añadio un nuevo sistema de gestion de comandos (por archivos).

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 client.on("ready", () => {
-    console.log(`${client.user.tag} inicio corractamente`);
-    console.log(`Su estado actual es ${client.user.presence.status}`);
-  });
+  console.log(`${client.user.tag} inicio corractamente`);
+  console.log(`Su estado actual es ${client.user.presence.status}`);
+});
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  // Filtrado de mensajes por el simbolo identificador de comandos
-  if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // Con SET agrego un nuevo elemento a la coleccion de comandos
+  // donde la clave es el nombre del comando y el valor es el modulo exportado
+  client.commands.set(command.name, command);
+}
 
-  // Parseado de argumentos de los comandos
-  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+client.on('message', message => {
+	if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
-  // Comandos de prueba
-  if(command === 'hola') {
-    message.channel.send(`Hola ${message.author.username}!`);
-  } else
-  if (command === 'help') {
-    message.channel.send(`Por ahora el unico comando que tengo habilitado es ${process.env.PREFIX}hola`);
-  }
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('Hubo un error al ejecutar el comando.');
+	}
 });
 
 client.login()
