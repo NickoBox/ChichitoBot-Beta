ac1c70a3391bd7bc5e586b4480b31c7ad0c3e676
NickoAcu <nickoacu@hotmail.com>
2021-05-18 00:25:49 -0300

    Update: El mensaje de inicio fue cambiado, ahora tambien indica el status

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('El bot esta en linea!')
-})
+client.on("ready", () => {
+    console.log(`${client.user.tag} inicio corractamente`);
+    console.log(`Su estado actual es ${client.user.presence.status}`);
+  });
 
 client.login()
