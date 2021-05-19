fecfeffd1508a3292023005e5242939adee6374f
NickoAcu <nickoacu@hotmail.com>
2021-05-19 04:41:33 -0300

    Fix: Se corriogio el nombre de la clave [description].

--- commands/moderation/role.js
+++ commands/moderation/role.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'role',
-    desctiption: 'Asigna un rol a un usuario.',
+    description: 'Asigna un rol a un usuario.',
     guildOnly: true,
 	args: true,
 	usage: '<usuario> <rol>',
