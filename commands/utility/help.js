4fe709600f85f43406ea3f6eb799e5f8dba2ebb3
NickoAcu <nickoacu@hotmail.com>
2021-05-19 09:10:19 -0300

    Add: Se agrego un nuevo comando [help].

---
+++ commands/utility/help.js
@@ -0,0 +1,44 @@
+module.exports = {
+	name: 'help',
+	description: 'List all of my commands or info about a specific command.',
+	aliases: ['ayuda', 'comandos', 'commands'],
+	usage: '[nombre de comando]',
+	cooldown: 5,
+	execute(message, args) {
+		const data = [];
+		const { commands } = message.client;
+
+		if (!args.length) {
+			data.push('Aquí tienes una lista de mis comandos:');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nPuedes usar \`${prefix}help [nombre de comando]\` para obtener informacion especifica de cada comando.`);
+
+			return message.author.send(data, { split: true })
+				.then(() => {
+					if (message.channel.type === 'dm') return;
+					message.reply('Te envie un mensaje privaco con todos mis comandos.');
+				})
+				.catch(error => {
+					console.error(`No se pudo enviar DM a ${message.author.tag}.\n`, error);
+					message.reply('Parece que no puedo enviarte mensajes privados!');
+				});
+		}
+
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+		if (!command) {
+			return message.reply('ese no es un comando valido!');
+		}
+
+		data.push(`**Nombre:** ${command.name}`);
+
+		if (command.aliases) data.push(`**Alias:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Descripcion:** ${command.description}`);
+		if (command.usage) data.push(`**Modo de uso:** ${prefix}${command.name} ${command.usage}`);
+
+		data.push(`**Enfriamiento:** ${command.cooldown || 3} segundo(s)`);
+
+		message.channel.send(data, { split: true });
+	},
+};
