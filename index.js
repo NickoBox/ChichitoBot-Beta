2eb2bd0a3506ebca9e3e81da4ac4b4b56c9ddbfa
NickoAcu <nickoacu@hotmail.com>
2021-05-19 05:12:39 -0300

    Update: Se agrego soporte para cooldown de comandos.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
 
 client.on("ready", () => {
   console.log(`${client.user.tag} inicio corractamente`);
@@ -44,6 +45,28 @@ client.on('message', message => {
   
     return message.channel.send(reply);
   }
+
+  const { cooldowns } = client;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+  
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+  
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`Por favor espera ${timeLeft.toFixed(1)} segundo(s) antes de usar \`${command.name}\` otravez.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
   try {
     command.execute(message, args);
