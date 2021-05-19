ec20df4bc5b5a29d604521adf88486e9668a42de
NickoAcu <nickoacu@hotmail.com>
2021-05-19 04:04:47 -0300

    Update: Se elimino el condicional

--- commands/fun/hola.js
+++ commands/fun/hola.js
@@ -2,9 +2,6 @@ module.exports = {
 	name: 'hola',
 	description: 'El bot te saludara!',
 	execute(message, args) {
-		if (message.author.username == 'Lu Murga') {
-			return message.channel.send('PERO QUE DICE SEÑORAAA?');
-		}
-		return message.channel.send(`Hola ${message.author}`);
+		message.channel.send(`Hola ${message.author}`);
 	},
 };
